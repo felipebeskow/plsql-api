134dacc31cd407bc0ea0564403d5cc5a206ebdc6
felipebeskow <felipebeskow@outlook.com>
2022-12-12 22:31:13 -0300

    Criado projeto em express e rota ficha

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// Using a fixed Oracle time zone helps avoid machine and deployment differences
+process.env.ORA_SDTZ = 'UTC';
+
+const fs = require('fs');
+const oracledb = require('oracledb');
+const dbConfig = require('./dbconfig.js');
+
+// On Windows and macOS, you can specify the directory containing the Oracle
+// Client Libraries at runtime, or before Node.js starts.  On other platforms
+// the system library search path must always be set before Node.js is started.
+// See the node-oracledb installation documentation.
+// If the search path is not correct, you will get a DPI-1047 error.
+let libPath;
+if (process.platform === 'win32') {           // Windows
+  libPath = 'D:\\Oracle\\instantclient_21_7';
+} else if (process.platform === 'darwin') {   // macOS
+  libPath = process.env.HOME + '/Downloads/instantclient_19_8';
+}
+if (libPath && fs.existsSync(libPath)) {
+  oracledb.initOracleClient({ libDir: libPath });
+}
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/ficha', require('./routes/api/ficha'));
+
+module.exports = app;
---
+++ routes/api/ficha.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+const oracledb = require('oracledb');
+const dbConfig = require('./../../dbconfig.js');
+
+/* POST ficha listing. */
+router.post('/', async function(req, res, next) {  
+  let connection;
+  let mensagem = {};
+  try {
+    connection = await oracledb.getConnection(dbConfig);
+    let result = await connection.execute(
+      `CALL PKG_FICHA.INSERE('${req.body.nome}','${req.body.cpf}','${req.body.email}','${req.body.telefone}','${req.body.cidade}','${req.body.uf}')`,
+      [],
+      { autoCommit: true } 
+    );
+    mensagem = {
+      mensagem:"Inserido com sucesso"
+    }
+  } catch (error) {
+    console.error(error);
+    res.json({
+      mensagem:error
+    });
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (error) {
+        console.error(error);
+        res.json({
+          mensagem:error
+        });
+      }
+    }
+  }
+
+  res.json( mensagem );
+  
+});
+
+module.exports = router;
